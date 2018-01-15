5ccbdf583d643fa8cc93036bb4875212da55b18c
irakliy01 <irakliy.kukava@gmail.com>
2018-01-16 02:22:39 +0300

    Set PostgreSQL db

--- shmot/shmot/urls.py
+++ shmot/shmot/urls.py
@@ -17,6 +17,6 @@ from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
-    path('main/', include('shmotapp.urls')),
+    path('', include('shmotapp.urls')),
     path('admin/', admin.site.urls),
 ]
