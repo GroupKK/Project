0ea4ccb73c74bef2dc7e304893ecaa9e107b365c
irakliy01 <irakliy.kukava@gmail.com>
2018-02-08 02:32:17 +0300

    Add decorator require_safe

--- src/shmot/mainapp/views.py
+++ src/shmot/mainapp/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import redirect, render
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.views.decorators.http import require_safe
+
 
 
 def redirect_to_home(request):
     return redirect('/')
 
 
+@require_safe
 def index(request):
     context = {}
     return render(request, 'index.html', context)
