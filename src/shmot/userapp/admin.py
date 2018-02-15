226e8d1b61044c71b02595487d963a5338af0df8
irakliy01 <irakliy.kukava@gmail.com>
2018-02-16 02:49:00 +0300

    Add user model to admin panel

--- src/shmot/userapp/admin.py
+++ src/shmot/userapp/admin.py
@@ -1,16 +1,39 @@
-"""
 from django.contrib import admin
-from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser
+from .models import Profile
 
-class CustomUserAdmin(admin.ModelAdmin):
-    model = CustomUser
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
+class ProfileInLine(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+    
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInLine, )
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'get_phonenumber', 'get_city', 'get_numberofposts')
+    list_select_related = ('profile', )
 
-admin.site.register(CustomUser, CustomUserAdmin)
+    def get_phonenumber(self, instance):
+        return instance.profile.phone_number
+    get_phonenumber.short_description = 'Phone Number'
+    
+    def get_city(self, instance):
+        return instance.profile.city
+    get_city.short_description = 'City'
+    
+    def get_numberofposts(self, instance):
+        return instance.profile.number_of_posts
+    get_numberofposts.short_description = 'Number of posts'
+    
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
 
-"""
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+
+
