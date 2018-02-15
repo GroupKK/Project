cfa06c96b7dd9e42475f9d99128577a9ea017d81
irakliy01 <irakliy.kukava@gmail.com>
2018-02-16 02:05:35 +0300

    Extend User model

--- src/shmot/userapp/models.py
+++ src/shmot/userapp/models.py
@@ -1,10 +1,24 @@
-"""
-from django.contrib.auth.models import AbstractUser, UserManager
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-class CustomUserManager(UserManager):
-    pass
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.IntegerField()
+    city = models.CharField(max_length=15)
+    number_of_posts = models.IntegerField()
+    # posts
+    # liked_posts
+    # favourite_posts
 
-class CustomUser(AbstractUser):
-    objects = CustomUserManager()
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
 
-"""
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+    
+
